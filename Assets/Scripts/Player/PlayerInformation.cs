49f4b482a5b5ef4048f348ce29da78d28e4d0c27
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2024-04-14 01:48:26 -0400

    added create user request to api

--- Assets/Scripts/Player/PlayerInformation.cs
+++ Assets/Scripts/Player/PlayerInformation.cs
@@ -1,4 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class PlayerInformation : MonoBehaviour
 {
@@ -29,6 +32,33 @@ public class PlayerInformation : MonoBehaviour
 			// Generate a new unique ID and save to player prefs
 			userID = System.Guid.NewGuid().ToString();
 			PlayerPrefs.SetString("UserID", userID);
+			StartCoroutine(CreateUser(userID));
+		}
+	}
+
+	private IEnumerator CreateUser(string playerId)
+	{
+		string url = $"http://localhost:5000/create";
+		string jsonData = $"{{ \"_id\": ${playerId}, \"username\": ${playerId}}}";
+
+		Debug.Log("Sending JSON: " + jsonData); // Log the JSON being sent
+		using (UnityWebRequest request = new UnityWebRequest(url, "POST"))
+		{
+			byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
+			request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+			request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+			request.SetRequestHeader("Content-Type", "application/json");
+
+			yield return request.SendWebRequest();
+
+			if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+			{
+				Debug.LogError(request.error);
+			}
+			else
+			{
+				Debug.Log("Response: " + request.downloadHandler.text);
+			}
 		}
 	}
 }
